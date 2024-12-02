c6a3af3dc9e4c07947bb5f4b796aadb5ae6bd4a5
Haena <gogohaena@gmail.com>
2024-12-02 10:34:34 +0900

    서킷 브레이커 적용

--- product/src/main/java/com/sparta/msa_exam/product/ProductController.java
+++ product/src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -1,7 +1,5 @@
 package com.sparta.msa_exam.product;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
-	@Value("${server.port}")
-	private String serverPort;
+	private final ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
 
 	@GetMapping("/{id}")
-	public String getProduct(@PathVariable("id") String id) {
-		return "Product Id: " + id + ", Server Port: " + serverPort;
+	public Product getProduct(@PathVariable("id") String id) {
+		return productService.getProductDetails(id);
 	}
+
+	// @Value("${server.port}")
+	// private String serverPort;
+	//
+	// @GetMapping("/{id}")
+	// public String getProduct(@PathVariable("id") String id) {
+	// 	return "Product Id: " + id + ", Server Port: " + serverPort;
+	// }
 }
 
